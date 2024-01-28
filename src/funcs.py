01b5e03c4378918cb23af80cae0fa03ab805312d
Asya <sillestyna@gmail.com>
2024-01-28 18:05:57 +0500

    all tests added

--- src/funcs.py
+++ src/funcs.py
@@ -19,7 +19,6 @@ def cleaning_data(data):
         else:
             new_data.append(transaction)
     return new_data
-<<<<<<< HEAD
 
 
 def date_form(date_in):
@@ -27,8 +26,17 @@ def date_form(date_in):
     return date_out
 
 
+def reformating_num(bill_or_card):
+    data_trnsctn = bill_or_card.split(" ")
+    bill_or_card_name = ' '.join([element for element in data_trnsctn if not element.isdigit()])
+    bill_or_card_num = data_trnsctn[-1]
+    if data_trnsctn[0] == 'Счет':
+        return bill_or_card_name + ' **' + bill_or_card_num[-4:]
+    return bill_or_card_name + ' ' + bill_or_card_num[:4] + ' ' + bill_or_card_num[
+                                                                  4:6] + '** ' + '**** ' + bill_or_card_num[-4:]
+
+
 def outputting_data(some_dict):
     return (f"{date_form(some_dict['date'])}, {some_dict['description']}\n"
             f"{some_dict.get('from')} -> {some_dict['to']}\n"
             f"{some_dict['operationAmount']['amount']} {some_dict['operationAmount']['currency']['name']} \n")
-
